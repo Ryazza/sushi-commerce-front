49c2302f88853e08b46f52f76b9b7e49abf74ff5
Emeline <marie.le-du@epitech.eu>
2021-07-16 11:40:05 +0200

    create component connexion

---
+++ src/Components/Connexion/connexion.js
@@ -0,0 +1,13 @@
+import React, {Component, Fragment} from 'react';
+import './connexion.css';
+
+
+export default class Connexion extends Component {
+    render() {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        )
+    }
+}
