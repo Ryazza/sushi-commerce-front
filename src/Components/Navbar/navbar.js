7dbfa73697794a8df5f374cb75de8e92194a5774
Thierry <thierry.drouin@epitech.eu>
2021-07-16 12:17:03 +0200

    Visuel navBar ✔️

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,4 +1,6 @@
 import React, {Component, Fragment} from 'react';
+import {Link} from "react-router-dom";
+
 import './navbar.css';
 
 class Navbar extends Component {
@@ -6,44 +8,36 @@ class Navbar extends Component {
     render() {
         return (
             <Fragment>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navBar_mainContainer">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Navbar</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
+                        <a href="#" className="navbar-brand p-0"><img src={process.env.PUBLIC_URL + '/assets/logo.png'} className="navBar_logo" alt=""/></a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"> </span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Link</a>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown
-                                    </a>
-                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="#">Action</a></li>
-                                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                                        <li>
-                                            <hr className="dropdown-divider"/>
-                                        </li>
-                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                    </ul>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-                                </li>
-                            </ul>
-                            <form className="d-flex">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                            </form>
+                        <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+                            <div className="nav-item"> </div>
+                            <div className="nav-item">
+                                <form>
+                                    <div className="form-floating ms-3 me-3">
+                                        <input type="text" className="form-control navBar_searchBar ps-0 pe-0" id="floatingSearch" placeholder="Password"/>
+                                        <label htmlFor="floatingSearch" className="font_lobster navBar_searchBar--label ps-0 pe-0 pt-0">Rechercher un produit, une marque, une référence…</label>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="nav-item">
+                                <Link className="btn btn-default navBar_link font_lobster" to="/login">
+                                    Mon Compte
+                                </Link>
+                                <Link className="btn btn-default navBar_link font_lobster" to="/panier">
+                                    Mon panier
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </nav>
+                <div className="container-fluid sousNavTemporaire">
+                </div>
             </Fragment>
         )
     }
