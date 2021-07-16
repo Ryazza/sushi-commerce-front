ebe4872ab0cf5840b6569bed09d6f571e4d4960f
Thierry <thierry.drouin@epitech.eu>
2021-07-17 00:37:22 +0200

    Switch de composant et création des sous composant Terminé

---
+++ src/Components/MyAccount/Adress/adress.js
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from 'react';
+
+import './me.css';
+
+class MyAccount extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Fragment>
+                <p>me components works</p>
+            </Fragment>
+        )
+    }
+}
+
+export default MyAccount
---
+++ src/Components/MyAccount/Ordered/ordered.js
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from 'react';
+
+import './adress.css';
+
+class Adress extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Fragment>
+                <p>adress components works</p>
+            </Fragment>
+        )
+    }
+}
+
+export default Adress
