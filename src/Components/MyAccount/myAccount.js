314940189e263f021fb1a1ea1920fcb97d0b8295
Thierry <thierry.drouin@epitech.eu>
2021-07-17 00:11:20 +0200

    Ajout de tout les bouton pour correspondre au switch de composant  ✔️

--- src/Components/MyAccount/myAccount.js
+++ src/Components/MyAccount/myAccount.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react';
+import Me from "./Me/me"
 
 import './myAccount.css';
 
@@ -7,10 +8,27 @@ class MyAccount extends Component {
     constructor(props) {
         super(props);
         this.state = {me: true, adress: false, ordered: false, panier: false}
+
+        this.handleChangeForMe = this.handleChangeForMe.bind(this);
+        this.handleChangeForAdress = this.handleChangeForAdress.bind(this);
+        this.handleChangeForOrdered = this.handleChangeForOrdered.bind(this);
+        this.handleChangeForPanier = this.handleChangeForPanier.bind(this);
     }
 
-    componentDidMount() {
+    handleChangeForMe() {
+        this.setState({me: true, adress: false, ordered: false, panier: false})
+    }
 
+    handleChangeForAdress() {
+        this.setState({me: false, adress: true, ordered: false, panier: false})
+    }
+
+    handleChangeForOrdered() {
+        this.setState({me: false, adress: false, ordered: true, panier: false})
+    }
+
+    handleChangeForPanier() {
+        this.setState({me: false, adress: false, ordered: false, panier: true})
     }
 
     render() {
@@ -27,26 +45,26 @@ class MyAccount extends Component {
                             <div className="myAccount_me text-center">Nom Prénom</div>
                             <div className="text-center text-danger mt-2 mb-4"><img src={process.env.PUBLIC_URL + '/assets/icons8-fermer-96.png'} alt="Boutton de déconnection" class="myAccount_img--disconnect"/> Me déconnecter</div>
                             <div className="row myAccount_separatorMenu">
-                                <button className="btn btn-default text-center pt-2 pb-2">Mes informations</button>
+                                <button className="btn btn-default text-center pt-2 pb-2" onClick={this.handleChangeForMe}>Mes informations</button>
                             </div>
                             <div className="row myAccount_separatorMenu">
-                                <button className="btn btn-default text-center pt-2 pb-2">Mes adresses</button>
+                                <button className="btn btn-default text-center pt-2 pb-2" onClick={this.handleChangeForAdress}>Mes adresses</button>
                             </div>
                             <div className="row myAccount_separatorMenu">
-                                <button className="btn btn-default text-center pt-2 pb-2">Mes commandes</button>
+                                <button className="btn btn-default text-center pt-2 pb-2" onClick={this.handleChangeForOrdered}>Mes commandes</button>
                             </div>
                             <div className="row myAccount_separatorMenu">
-                                <button className="btn btn-default text-center pt-2 pb-2">Mon panier</button>
+                                <button className="btn btn-default text-center pt-2 pb-2" onClick={this.handleChangeForPanier}>Mon panier</button>
                             </div>
                         </div>
                         <div className="col-8">
                             {(() => {
                                 switch (true) {
-                                    case me:   return "#FF0000";
+                                    case me:   return (<Me />);
                                     case adress: return "#00FF00";
                                     case ordered:  return "#0000FF";
                                     case panier:  return "#0000FF";
-                                    default:      return "#FFFFFF";
+                                    default:      return (<Me />);
                                 }
                             })()}
                         </div>
