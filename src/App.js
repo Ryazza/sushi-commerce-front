c987e613a9ad152f3281f8883f3c7b2018c29a41
Marie <marie.le-du@epitech.eu>
2021-07-18 22:47:11 +0200

    dossier test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Bus from "./Utils/Bus";
 import Navbar from './Components/Navbar/navbar'
 import Register from './Components/Register/register'
 import MyAccount from './Components/MyAccount/myAccount'
-
 import Connexion from "./Components/Connexion/connexion";
+import ConnexionTest from "./Components/ConnexionTest/connexionTest";
 
 window.flash = (title, message, type = "success") => Bus.emit ('flash', ({title, message, type}));
 
@@ -23,6 +23,12 @@ function App() {
                 </Route>
                 <Route path="/register" >
                     <Register />
+                </Route>
+                <Route path="/login" >
+                    <Connexion />
+                </Route>
+                <Route path="/loginTest" >
+                    <ConnexionTest />
                 </Route>
                 <Route path="/" >
                     <Navbar />
--- src/Components/Connexion/connexion.js
+++ src/Components/Connexion/connexion.js
@@ -2,8 +2,8 @@ import React, {Component, Fragment} from 'react';
 import './connexion.css';
 import {Link} from "react-router-dom";
 import logo from '../../Assets/logo.png';
-
 export default class Connexion extends Component {
+
     render() {
         return (
             <Fragment>
@@ -15,30 +15,31 @@ export default class Connexion extends Component {
                         </button>
                     </div>
                 </nav>
-                <div className="container col-6 font_Lobster" id="connexion">
-                    <button className="col-6 connexionBtn">
-                        Connexion
-                    </button>
-                    <button className="col-6 inscriptionBtn">
-                        Inscription
-                    </button>
-                    <form id="connexionForm">
-                        <div class="form-floating mb-3">
-                        <input type="email" className="form-control" id="formGroupEmailConnexion" placeholder="name@example.com"/>
-                            <label htmlFor="formGroupEmailConnexion">Adresse e-mail :</label>
+
+                <div className="container ">
+                    <div className="row justify-content-center">
+                        <div className="col-6 Login_main mt-5 mb-5 pb-5">
+                            <div className="row font_lobster text-center fs-2">
+                                <div className="col-6 Login_connectBtn global_fontColor--cream pt-2 pb-2" >Connexion</div>
+                                <Link className="col-6 Login_registerBtn global_fontColor--cream pt-2 pb-2" to="/register">Inscription</Link>
+                            </div>
+                            <div className="row justify-content-center mt-4">
+                                <div className="col-8 pt-3 pb-3">
+                                    <div className="form-floating mb-4">
+                                        <input type="email" className="form-control global_bgColor--blackCyan Register_input ps-0 pe-0 text-white" id="Login_email" placeholder="name@example.com" onChange={this.handleChangeEmail}/>
+                                        <label htmlFor="Login_email" className="Register_label global_fontColor--cream ps-0 pe-0 pt-0">Adresse e-mail</label>
+                                    </div>
+                                    <div className="form-floating mb-4">
+                                        <input type="password" className="form-control global_bgColor--blackCyan Register_input ps-0 pe-0 text-white" id="Login_password" placeholder="• • • • • • • • •" onChange={this.handleChangePassword}/>
+                                        <label htmlFor="Login_password" className="Register_label global_fontColor--cream ps-0 pe-0 pt-0">Mot de passe</label>
+                                    </div>
+                                </div>
+                                <div className="row justify-content-center mt-3">
+                                    <button className="btn btn-default col-2 Login_btn" onClick={this.handleSubmitRegister}>Se connecter</button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="form-floating mb-3">
-                            <input type="password" className="form-control" id="formGroupPwdConnexion" placeholder="name@example.com"/>
-                            <label htmlFor="formGroupPwdConnexion">Mot de passe :</label>
-                        </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="checkbox" id="formGroupRememberMe"/>
-                                <label className="form-check-label" htmlFor="formGroupRememberMe">
-                                    Se rappeler de moi
-                                </label>
-                        </div>
-                        <input type="button" className="btn btn-primary " value="Se connecter"/>
-                    </form>
+                    </div>
                 </div>
             </Fragment>
         )
