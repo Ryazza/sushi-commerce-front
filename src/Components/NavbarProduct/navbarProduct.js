42ff365b1fad050dd344552ebe6f83187aa73fd3
Emeline <marie.le-du@epitech.eu>
2021-07-21 13:04:53 +0200

    create navbar product

---
+++ src/Components/NavbarProduct/navbarProduct.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {Component, Fragment} from "react";
+import {Link} from "react-router-dom";
+import './navbarProduct.css';
+
+export default class NavbarProduct extends Component {
+    render() {
+        return(
+            <Fragment>
+                <div className="Product_navBar container font_cabin d-flex justify-content-center">
+                    <p className="font_montserrat">Dashboard - Gestion des articles</p>
+                    <nav className="Product_navBar align-self-center">
+                        <Link className="Product_navBar_btn global_fontColor--whiteSmoke global_fontColorCTA" to="/newProduct">Créer</Link>
+                        <Link className="Product_navBar_btn global_fontColor--whiteSmoke global_fontColorCTA">Modifier</Link>
+                    </nav>
+                </div>
+            </Fragment>
+        )
+    }
+}
--- src/Views/Products/products.js
+++ src/Views/Products/products.js
@@ -3,12 +3,14 @@ import {Component, Fragment} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './products.css'
+import NavbarProduct from "../../Components/NavbarProduct/navbarProduct";
 
 export default class Products extends Component {
 
     render() {
         return(
             <Fragment>
+                <NavbarProduct/>
 
             </Fragment>
         )
