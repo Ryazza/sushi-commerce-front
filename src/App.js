5e1989bbe2c78d85a7c56168d6c389d92d11ac75
Thierry <thierry.drouin@epitech.eu>
2021-07-30 15:24:45 +0200

    Structure et préparation a la requete a venir OK

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
             <Router>
                 <Navbar/>
                 <Switch>
-                    <Route path="/category/souscategory/idProduit" component={ProductDetail}/>
+                    <Route path="/produit/idProduit" component={ProductDetail}/>
                     <Route path="/account" component={UserAccount}/>
                     <Route path="/" component={HomePage}/>
                 </Switch>
