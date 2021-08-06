00f76e2dc8835aec6597961d8f382e8c203a6626
Thierry <thierry.drouin@epitech.eu>
2021-08-06 10:19:11 +0200

    Changement de l'url pour l'api distante

--- src/Environment/environment.js
+++ src/Environment/environment.js
@@ -1,4 +1,4 @@
 export const environement = {
-    backBase: "http://localhost:4244",
-    backUser: "http://localhost:4244/user/"
+    backBase: "http://164.132.229.58:4244",
+    backUser: "http://164.132.229.58:4244/user/"
 }
