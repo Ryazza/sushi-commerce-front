3dc9cbdaa86b19073d22dd561fa0d8fa5bdfc161
jeje <jerome.botiveau@epitech.eu>
2021-08-06 12:46:37 +0200

    ligne inutile

--- src/Views/Admin/products/addProductPage.js
+++ src/Views/Admin/products/addProductPage.js
@@ -1,8 +1,6 @@
 import React from "react";
 import {Component, Fragment} from "react";
 import NavbarAdmin from "../../../Pioche/component/NavbarAdmin/navbarAdmin";
-// import AddCategory from "../../../Components/Admin/Labels/Category/addCategory";
-import AddProduct from "../../../Components/Admin/products/addProduct";
 import NewProduct from "../../../Components/Admin/products/newProduct";
 
 export default class AddProductPage extends Component {
