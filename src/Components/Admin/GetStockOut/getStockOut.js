c88db205ce52e374f7cd819421acd59bba07f243
Marie <marie.le-du@epitech.eu>
2021-08-08 18:14:32 +0200

    Ok

--- src/Components/Admin/GetStockOut/getStockOut.js
+++ src/Components/Admin/GetStockOut/getStockOut.js
@@ -20,8 +20,42 @@ export default class GetStockOut extends Component {
         }
         axios.get(environement.backBase+"/product/verifyStock", {headers:headers}).then(response => {
             this.setState({products: response.data.products})
+            console.log(response)
         })
+
     }
+    //
+    // handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     console.log("ok")
+    //     let canSend = false;
+    //     let quantity = document.getElementById('productsOut__quantity').value;
+    //     let productId = document.getElementById("productsOut__productId").value;
+    //     console.log(quantity, productId)
+    //     const headers = {
+    //         'Authorization': `Bearer ${AuthService.getCurrentAuth()}`
+    //     }
+    //     if(quantity > 0) {
+    //         canSend = true;
+    //         console.log(canSend)
+    //     }
+    //
+    //     if(canSend) {
+    //
+    //         axios.post(environement.backBase+"/product/updateStock/" , {
+    //
+    //         }, { headers: headers}).then( async res => {
+    //             this.setState({ success: res.data.message });
+    //             this.setState({ redirection: true });
+    //         }).catch( error => {
+    //             console.log( error.response );
+    //             if( error.response ) {
+    //                 this.setState({ errorMsg: error.response.data.message });
+    //             }
+    //         })
+    //     }
+    //
+    // }
 
     render() {
         return (
@@ -34,11 +68,11 @@ export default class GetStockOut extends Component {
                                     <thead>
                                     <tr>
                                         <th>#</th>
-                                        <td className={"hidden"}>id</td>
+                                        {/*<td className={"hidden"}>id</td>*/}
                                         <th>nom</th>
                                         <th>statut</th>
-                                        <th>quantité</th>
-                                        <th>actions</th>
+                                        {/*<th>quantité</th>*/}
+                                        {/*<th>actions</th>*/}
                                     </tr>
                                     </thead>
                                     <tbody>
@@ -47,11 +81,11 @@ export default class GetStockOut extends Component {
                                             return [
                                                 <tr key={index}>
                                                     <td>{index}</td>
-                                                    <td className={"hidden"}> {products._id}</td>
+                                                    {/*<td className={"hidden"} id={'productsOut__productId'}> {products._id}</td>*/}
                                                     <td>{products.name}</td>
                                                     <td><i className={"productsOut__rupture"}>Rupture Stock</i></td>
-                                                    <td><input type={'text'} placeholder={'indiquer une quantité'}/></td>
-                                                    <td><input type={"button"} value={'commander'}/></td>
+                                                    {/*<td><input type={'number'} placeholder={'indiquer une quantité'} id={"productsOut__quantity"} min={1}/></td>*/}
+                                                   {/*<td><input type={"button"} value={'commander'} className={"productsOut__order"+products._id} onClick={this.handleSubmit}/></td>*/}
                                                 </tr>
                                             ]
                                         }
