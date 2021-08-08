7e484b284fac53d463367036faf45e7f9ee31aa8
Marie <marie.le-du@epitech.eu>
2021-08-08 17:45:09 +0200

    requete get ok

--- src/Components/Admin/GetStockOut/getStockOut.js
+++ src/Components/Admin/GetStockOut/getStockOut.js
@@ -3,7 +3,7 @@ import {Component, Fragment} from "react";
 import './getStockOut.css';
 import {environement} from "../../../Environment/environment";
 import axios from "axios";
-import AuthService from "../../../../services/auth.service"
+import AuthService from "../../../services/auth.service"
 
 
 export default class GetStockOut extends Component {
@@ -22,4 +22,53 @@ export default class GetStockOut extends Component {
             this.setState({products: response.data.products})
         })
     }
+
+    render() {
+        return (
+            <Fragment>
+                {this.state.products && this.state.products.length !== 0 ?
+                    <form action="">
+                        <div className={"row"} id={"productsOut"}>
+                            <div className={"col-12"}>
+                                <table className={"table table-striped"}>
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <td className={"hidden"}>id</td>
+                                        <th>nom</th>
+                                        <th>statut</th>
+                                        <th>quantité</th>
+                                        <th>actions</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {this.state.products.map((products, index) => {
+                                        if(products.quantity === 0) {
+                                            return [
+                                                <tr key={index}>
+                                                    <td>{index}</td>
+                                                    <td className={"hidden"}> {products._id}</td>
+                                                    <td>{products.name}</td>
+                                                    <td><i className={"productsOut__rupture"}>Rupture Stock</i></td>
+                                                    <td><input type={'text'} placeholder={'indiquer une quantité'}/></td>
+                                                    <td><input type={"button"} value={'commander'}/></td>
+                                                </tr>
+                                            ]
+                                        }
+                                    })}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </form>
+
+                    :
+                    <div>
+                        <p className={"text-center mt-5"}>Erreur!</p>
+                    </div>
+                }
+            </Fragment>
+        );
+    }
+
 }
