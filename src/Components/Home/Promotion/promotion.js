a21dc963766f8b5300073f684bcfe06583673f64
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-08-18 13:13:34 +0200

    afficher les meilleures ventes globales sur home et sous-catégories

--- src/Components/Home/Promotion/promotion.js
+++ src/Components/Home/Promotion/promotion.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Component, Fragment} from "react";
+import MostSold from "../../Product/mostSold";
 
 export default class Promotion extends Component {
     render() {
         return(
             <Fragment>
-                coucou
+                <MostSold data={{name: "Lets Shop", id: "all"}}/>
+
             </Fragment>
         )
     }
